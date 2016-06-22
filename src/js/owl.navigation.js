639e8390ea408fb380b187649f679f7c8c74260f
Julian Tiemann <Jutiemann@gmail.com>
2016-06-22 11:48:05 +0200

    dist

--- src/js/owl.navigation.js
+++ src/js/owl.navigation.js
@@ -239,16 +239,16 @@
 
 			for (i = lower, j = 0, k = 0; i < upper; i++) {
 				var me = this;
-		    if(settings.autoWidth){
+		    if (settings.autoWidth) {
 	        var width = this.$element.width();
 	        var currentWidth = 0;
 	        var pageStart = lower;
 	        var start = 0;
 	        var end = 0;
 	        this.$element.find('.owl-item').each(function(i, e){
-            if(i > lower && i <= upper){
+            if (i > lower && i <= upper) {
               var itemWidth = $(e).width() + settings.margin;
-              if(currentWidth + itemWidth > width){
+              if (currentWidth + itemWidth > width) {
                 start = pageStart - lower;
                 end = i - 1 - lower;
                 me._pages.push({
@@ -262,7 +262,7 @@
               }
             }
 		      });
-	        if(end < upper){
+	        if (end < upper) {
             me._pages.push({
               start: end,
               end: upper
